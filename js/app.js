1d1032a2f35dae9e3af83b0a62d0c564e2279f38
K.Adam White <kadamwhite@gmail.com>
2013-12-08 22:49:55 -0500

    Render rectangles regularly onto the screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
 /* jshint devel:true */
 (function( $ ) {
+	var origin, screen, rectangle, rtl,
+		numX = 20,
+		now = new Date(),
+		$window = $( window );
+
+	screen = {
+		width: $window.width(),
+		height: $window.height()
+	};
+
+	rectangle = {
+		width: screen.width / numX, //150,
+		height: 250
+	};
+
+	origin = {
+		x: rectangle.width / 3, //screen.width / 3,
+		y: rectangle.height / 3 //screen.height / 2
+	};
+
+	function makeRect( index ) {
+		var offset, position,
+			$div = $('<div/>');
+
+		offset = {
+			x: index * rectangle.width / 2,
+			y: index * rectangle.height / 2
+		};
+
+		position = {
+			left: origin.x + offset.x + Math.random( now ) * 20 - 10,
+			top: origin.y + Math.random( now ) * 20 - 10
+		};
+
+		if ( rtl ) {
+			position.left = screen.width - position.left;
+		}
+
+		$div.css( position ).appendTo('body');
+	}
+
+	function makeRow( index ) {
+		origin = {
+			x: rectangle.width / 3,
+			y: rectangle.height / 3 + index * rectangle.height / 3
+		};
+
+		rtl = false;
+		_.times( numX, makeRect );
+		rtl = true;
+		_.times( numX, makeRect);
+	}
+
+	_.times( 20, makeRow );
 })( jQuery );
